fa7dc369717e4db9a7b0bc881299a9aa4baeb18a
Kasia <kasia-mural@wp.pl>
2018-01-11 12:28:24 +0100

    dodano komentarz

--- Main.java
+++ Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hello123");
+        System.out.println("Hello123"); // halo halo
     }
 }

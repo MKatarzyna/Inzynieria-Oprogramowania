2836d66d9627ed9447ccae8a9ddead5d4ed65a47
Kasia <kasia-mural@wp.pl>
2018-01-11 12:36:20 +0100

    wprowadzono zmiany od master

--- Main.java
+++ Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hello123"); // halo halo
+        System.out.println("Hello pisze jako master"); // halo halo
     }
 }
